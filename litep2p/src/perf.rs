a4b6f75782b955198774c2ebb3f69081284b20df
Alexandru Vasile <alexandru.vasile@parity.io>
2025-02-25 13:39:06 +0200

    litep2p: Implement user protocol for perf
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

--- litep2p/src/perf.rs
+++ litep2p/src/perf.rs
@@ -1,6 +1,15 @@
 
+use futures::StreamExt;
+
+use litep2p::{
+    codec::ProtocolCodec,
+    protocol::{Direction, TransportEvent, TransportService, UserProtocol},
+    substream::Substream,
+    PeerId, ProtocolName,
+};
 
 const PROTOCOL_NAME: &str = "/litep2p-perf/1.0.0";
+const LOG_TARGET: &str = "litep2p-perf";
 
 pub struct Perf {
     is_server: bool,
@@ -14,6 +23,46 @@ impl Perf {
             is_server,
             upload_bytes,
             download_bytes,
+        }
+    }
+}
+
+
+#[async_trait::async_trait]
+impl UserProtocol for Perf {
+    fn protocol(&self) -> ProtocolName {
+        PROTOCOL_NAME.into()
+    }
+
+    fn codec(&self) -> ProtocolCodec {
+        ProtocolCodec::Unspecified
+    }
+
+    async fn run(self: Box<Self>, mut service: TransportService) -> litep2p::Result<()> {
+        loop {
+            tokio::select! {
+                event = service.next() => match event {
+                    Some(TransportEvent::ConnectionEstablished { peer, .. }) => {
+                        if !self.is_server {
+                            service.open_substream(peer).unwrap();
+                        }
+                    }
+                    Some(TransportEvent::ConnectionClosed { peer }) => {
+                        tracing::debug!(target: LOG_TARGET, "connection closed: peer={}", peer);
+                    }
+                    Some(TransportEvent::SubstreamOpened {
+                        substream,
+                        direction,
+                        ..
+                    }) => match direction {
+                        Direction::Inbound => {
+                        }
+                        Direction::Outbound(_substream_id) => {
+                        }
+                    },
+                    _ => {},
+                },
+            }
         }
     }
 }
