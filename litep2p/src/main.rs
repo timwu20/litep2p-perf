bf7515648969a2c449aa2d60d888055a86dd8bd9
Alexandru Vasile <alexandru.vasile@parity.io>
2025-03-05 14:22:32 +0200

    litep2p: Construct the performance in substream mode
    Signed-off-by: Alexandru Vasile <alexandru.vasile@parity.io>

--- litep2p/src/main.rs
+++ litep2p/src/main.rs
@@ -1,4 +1,3 @@
-
 use clap::Parser as ClapParser;
 
 use utils::Command;
@@ -13,7 +12,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let command = Command::parse();
 
-    let mut litep2p = match command {
+    let (perf_client, server_address) = match command {
         Command::Server(server_opts) => {
             let perf = Box::new(perf::Perf::new(perf::PerfMode::Server));
 
@@ -39,37 +38,45 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                 .with_user_protocol(perf)
                 .build();
 
-            let litep2p = litep2p::Litep2p::new(litep2p_config)?;
+            let mut litep2p = litep2p::Litep2p::new(litep2p_config)?;
 
             let listen_addr: Vec<_> = litep2p.listen_addresses().collect();
             tracing::info!("Server listening on address: {listen_addr:?}");
 
-            litep2p
+            while let Some(event) = litep2p.next_event().await {
+                tracing::info!("Event: {event:?}");
+            }
+
+            return Ok(());
         }
-        Command::Client(client_opts) => {
-            let perf = Box::new(perf::Perf::new(perf::PerfMode::Client {
+
+        Command::Client(client_opts) => (
+            Box::new(perf::Perf::new(perf::PerfMode::Client {
                 upload_bytes: client_opts.upload_bytes as u64,
                 download_bytes: client_opts.download_bytes as u64,
-            }));
-
-            let litep2p_config = litep2p::config::ConfigBuilder::new()
-                .with_tcp(litep2p::transport::tcp::config::Config {
-                    reuse_port: true,
-                    nodelay: true,
-                    ..Default::default()
-                })
-                .with_user_protocol(perf)
-                .build();
-
-            let mut litep2p = litep2p::Litep2p::new(litep2p_config)?;
-
-            litep2p
-                .dial_address(client_opts.server_address.parse()?)
-                .await?;
-
-            litep2p
-        }
+            })),
+            client_opts.server_address,
+        ),
+        Command::ClientSubstream(client_opts) => (
+            Box::new(perf::Perf::new(perf::PerfMode::ClientSubstream {
+                substreams: client_opts.substreams,
+            })),
+            client_opts.server_address,
+        ),
     };
+
+    let litep2p_config = litep2p::config::ConfigBuilder::new()
+        .with_tcp(litep2p::transport::tcp::config::Config {
+            reuse_port: true,
+            nodelay: true,
+            ..Default::default()
+        })
+        .with_user_protocol(perf_client)
+        .build();
+
+    let mut litep2p = litep2p::Litep2p::new(litep2p_config)?;
+
+    litep2p.dial_address(server_address.parse()?).await?;
 
     while let Some(event) = litep2p.next_event().await {
         tracing::info!("Event: {event:?}");
